cf46b6d765583138d93065ac6828676dd2f256b4
lockfish <lockfish@users.noreply.github.com>
2025-04-23 21:44:46 -0400

    Update pol_job.sh

--- Ce/pol_job.sh
+++ Ce/pol_job.sh
@@ -7,11 +7,6 @@
 #SBATCH --partition=thal5
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=1G
-#SBATCH --time=0-04:00:00
+#SBATCH --time=0-12:00:00
 
-python pol_rixs_E.py
-python pol_denom_E.py
-python pol_FQ_E.py
-python pol_rixs_M.py
-python pol_denom_M.py
-python pol_FQ_M.py
+python qfi_pol_Ce.py
